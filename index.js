e5f21cb8a551eac4fdb681a50e1b7657a80d25c3
sruthi-lal <lalsruthi145@gmail.com>
2023-11-28 18:00:31 +0530

    add js

---
+++ index.js
@@ -0,0 +1,24 @@
+// let getElement =  document.getElementsByTagName("p");
+
+// console.log(getElement.length)
+
+
+// let getID  = document.getElementById("jsCode")
+// console.log(getID)
+
+// let getClass = document.getElementsByClassName("paraClass")
+// console.log(getClass)
+
+// let getQuerySelector = document.querySelectorAll("li")
+// console.log(getQuerySelector)
+
+
+
+const para = document.querySelector('p')
+console.log(para.innerText)
+
+const parahtml = document.querySelector('p')
+console.log(parahtml.innerHTML)
+
+const paraContent = document.querySelector('p')
+console.log(paraContent.textContent)
